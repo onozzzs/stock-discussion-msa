98066dd9b09e51c33ad2f4af53323af3de78bddf
jmwoo <dnwlals00@gmail.com>
2024-02-16 18:03:49 +0900

    feat: keyword 별 오름차순, 내림차순 정렬

--- stock-service/src/main/java/com/example/stock/controller/StockController.java
+++ stock-service/src/main/java/com/example/stock/controller/StockController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,8 +18,10 @@ public class StockController {
     private StockService stockService;
 
     @GetMapping
-    public ResponseEntity<?> page(Pageable pageable) {
-        Page<DailyStock> stockPage = stockService.findAll(pageable);
+    public ResponseEntity<?> page(@RequestParam int page,
+                                  @RequestParam(defaultValue = "id") String sortBy,
+                                  @RequestParam(defaultValue = "desc") String order) {
+        Page<DailyStock> stockPage = stockService.findAll(page, sortBy, order);
         return ResponseEntity.ok().body(stockPage);
     }
 
--- stock-service/src/main/java/com/example/stock/service/StockService.java
+++ stock-service/src/main/java/com/example/stock/service/StockService.java
@@ -5,23 +5,36 @@ import com.example.stock.model.DailyStock;
 import com.example.stock.repository.DailyStockRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class StockService {
+    @Value("${spring.data.web.pageable.default-page-size}")
+    private int PAGE_SIZE;
+
     @Autowired
     private DailyStockRepository dailyStockRepository;
 
-    public Page<DailyStock> findAll(Pageable pageable) {
+    public Page<DailyStock> findAll(int page, String sortBy, String order) {
+        Sort sort;
+        if (order.equals("desc")) {
+            sort = Sort.by(Sort.Order.desc(sortBy));
+        } else {
+            sort = Sort.by(Sort.Order.asc(sortBy));
+        }
+
+        Pageable pageable = PageRequest.of(page, PAGE_SIZE, sort);
         return dailyStockRepository.findAll(pageable);
     }
 
     public Page<DailyStock> search(SearchDTO searchDTO) {
-        Pageable pageable = PageRequest.of(searchDTO.getPage(), 5);
+        Pageable pageable = PageRequest.of(searchDTO.getPage(), PAGE_SIZE);
         if (searchDTO.getType().equals("name")) {
             return dailyStockRepository.findByStockNameContaining(pageable, searchDTO.getKeyword());
         }
